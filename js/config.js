cb5e25e7c18d388069dc498e4a9e450d6d719a69
unis0911 <2567832345@qq.com>
2025-09-03 13:46:02 +0800

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -13,8 +13,8 @@ const PASSWORD_CONFIG = {
 
 // 网站信息配置
 const SITE_CONFIG = {
-    name: 'LibreTV',
-    url: 'https://libretv.is-an.org',
+    name: 'LONGTTV',
+    url: 'https://libretv-e45.pages.dev/',
     description: '免费在线视频搜索与观看平台',
     logo: 'image/logo.png',
     version: '1.0.3'
