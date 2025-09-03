9a8859219d81eeaf6a62b6fe72ba780f95cda719
unis0911 <2567832345@qq.com>
2025-09-03 13:23:17 +0800

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -22,9 +22,9 @@ const SITE_CONFIG = {
 
 // API站点配置
 const API_SITES = {
-    testSource: {
-        api: 'https://www.example.com/api.php/provide/vod',
-        name: '空内容测试源',
+    dyttzy: {
+        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
+        name: 'DYTT',
         adult: true
     }
     //ARCHIVE https://telegra.ph/APIs-08-12
