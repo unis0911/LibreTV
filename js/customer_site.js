3e60dc08b9e0fc153fd8e3cc86407321930b2499
unis0911 <2567832345@qq.com>
2025-09-03 13:51:17 +0800

    Add files via upload

--- js/customer_site.js
+++ js/customer_site.js
@@ -3,14 +3,6 @@ const CUSTOMER_SITES = {
         api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
         name: 'DYTT',
     }
-    mym3u8: {
-        api: 'https://api.maoyanapi.top/api.php/provide/vod',
-        name: 'MY',
-    }
-    MZ: {
-        api: 'https://mozhuazy.com/api.php/provide/vod/?ac=list',
-        name: 'MZ',
-    }
 };
 
 // 调用全局方法合并
