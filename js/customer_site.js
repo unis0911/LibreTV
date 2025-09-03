cd4ebcfb28ae8ef8db48a2426c3d0f472af7af31
unis0911 <2567832345@qq.com>
2025-09-03 16:35:29 +0800

    Add files via upload

--- js/customer_site.js
+++ js/customer_site.js
@@ -4,11 +4,7 @@ const CUSTOMER_SITES = {
         name: 'MY',
     }
 };
-    MZ: {
-        api: 'https://mozhuazy.com/api.php/provide/vod',
-        name: 'MZ',
-    }
-};
+
 // 调用全局方法合并
 if (window.extendAPISites) {
     window.extendAPISites(CUSTOMER_SITES);
